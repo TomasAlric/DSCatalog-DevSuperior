e024a6f30d4fb1f0caea70f4d43e6e2e34944f9b
TomasAlric <tomas.alricc@gmail.com>
2022-02-09 10:01:40 -0300

    Inserindo categoria

--- dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -16,19 +16,27 @@ import com.devsuperior.dscatalog.repositories.CategoryRepository;
 @Service
 public class CategoryService {
 
-	@Autowired
-	private CategoryRepository repository;
+    @Autowired
+    private CategoryRepository repository;
 
-	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-	}
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
 
-	@Transactional(readOnly = true)
-	public CategoryDTO findById(Long id) {
-		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-		return new CategoryDTO(entity);
-	}
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
 }
