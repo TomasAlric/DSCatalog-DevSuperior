e474a2ffcbf24d9854423e9236571f583d5646fa
TomasAlric <tomas.alricc@gmail.com>
2022-03-02 01:22:24 -0300

    Atualizando o getOne por getById

--- dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -54,7 +54,7 @@ public class ProductService {
     public ProductDTO update(Long id, ProductDTO dto) {
 
         try {
-            Product entity = repository.getOne(id);
+            Product entity = repository.getById(id);
             copyDtoToEntity(dto,entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
@@ -84,7 +84,7 @@ public class ProductService {
 
         entity.getCategories().clear();
         for(CategoryDTO catDto : dto.getCategories()){
-            Category category = categoryRepository.getOne(catDto.getId());
+            Category category = categoryRepository.getById(catDto.getId());
             entity.getCategories().add(category);
         }
     }
