30449dc56ee218f6ca00fa491c44295970d352ae
TomasAlric <tomas.alricc@gmail.com>
2022-03-08 18:06:26 -0300

    Lista de Product associada a Category

--- dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ dscatalog/back-end/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,7 +2,9 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.*;
 
@@ -21,6 +23,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -63,6 +68,10 @@ public class Category implements Serializable {
     @PreUpdate
     public void preUpdate(){
         updatedAt = Instant.now();
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
